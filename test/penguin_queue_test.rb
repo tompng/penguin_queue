a9a72faa2c04e75f37773f12c2ab85a3ee864fe6
tompng <tomoyapenguin@gmail.com>
2017-02-20 23:23:16 +0900

    test var rename

--- test/penguin_queue_test.rb
+++ test/penguin_queue_test.rb
@@ -6,64 +6,64 @@ class PenguinQueueTest < Minitest::Test
   end
 
   def test_enq_deq
-    h = PenguinQueue.new
-    10.times{|i|h.enq i.to_s}
-    assert h.empty? == false
-    assert h.size == 10
-    assert 10.times.map{h.deq} == 10.times.map(&:to_s)
-    assert h.empty? == true
-    assert h.size == 0
+    q = PenguinQueue.new
+    10.times{|i|q.enq i.to_s}
+    assert q.empty? == false
+    assert q.size == 10
+    assert 10.times.map{q.deq} == 10.times.map(&:to_s)
+    assert q.empty? == true
+    assert q.size == 0
   end
 
   def test_priority
-    h = PenguinQueue.new
-    nodes = 13.times.map{|i|h.enq i, priority: 13*(i+1)%13}
-    assert h.first_with_priority == [h.first_node.value, h.first_node.priority]
-    assert h.first_node.priority == 0
+    q = PenguinQueue.new
+    nodes = 13.times.map{|i|q.enq i, priority: 13*(i+1)%13}
+    assert q.first_with_priority == [q.first_node.value, q.first_node.priority]
+    assert q.first_node.priority == 0
     nodes.each{|n|n.priority = n.value}
-    assert 13.times.map{h.deq} == 13.times.to_a
+    assert 13.times.map{q.deq} == 13.times.to_a
   end
 
   def test_same_priority
-    h = PenguinQueue.new
-    10.times{|i|h.enq i, priority: 0}
-    arr1 = 5.times.map{h.deq}
-    arr2 = h.deq 2
-    arr3 = h.deq 3
+    q = PenguinQueue.new
+    10.times{|i|q.enq i, priority: 0}
+    arr1 = 5.times.map{q.deq}
+    arr2 = q.deq 2
+    arr3 = q.deq 3
     arr = arr1+arr2+arr3
     assert arr == arr.sort
   end
 
   def test_node_remove
-    h = PenguinQueue.new
-    nodes = 30.times.to_a.shuffle.map{|i|h<<i}.shuffle.each{|n|
+    q = PenguinQueue.new
+    nodes = 30.times.to_a.shuffle.map{|i|q<<i}.shuffle.each{|n|
       n.remove if n.priority%3==1
-      h.remove n if n.priority%3==2
+      q.remove n if n.priority%3==2
     }
-    assert h.size.times.map{h.deq} == 30.times.select{|i|i%3==0}
+    assert q.size.times.map{q.deq} == 30.times.select{|i|i%3==0}
   end
 
   def test_priority_proc
-    h = PenguinQueue.new{|v|-v.to_i}
-    h.push(*20.times.map(&:to_s).shuffle)
-    assert 10.times.map{h.deq} == (10...20).map(&:to_s).reverse
+    q = PenguinQueue.new{|v|-v.to_i}
+    q.push(*20.times.map(&:to_s).shuffle)
+    assert 10.times.map{q.deq} == (10...20).map(&:to_s).reverse
   end
 
   def test_invalid_compare
-    h = PenguinQueue.new
-    h << 'a'
+    q = PenguinQueue.new
+    q << 'a'
     err = :compare_error
-    b = h << 'b' rescue err
-    c = h << :c rescue err
+    b = q << 'b' rescue err
+    c = q << :c rescue err
     assert b != err
     assert c == err
   end
 
   def test_value_update
-    h = PenguinQueue.new
-    h.push(*10.times)
-    assert h.first == 0
-    h.first_node.value = 1
-    assert h.first == 1
+    q = PenguinQueue.new
+    q.push(*10.times)
+    assert q.first == 0
+    q.first_node.value = 1
+    assert q.first == 1
   end
 end
